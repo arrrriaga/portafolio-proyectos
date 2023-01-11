8f2a101a8aea47e03e64d08404110e4d2e163c87
Francisco Arriaga Velasco <94820722+arrrriaga@users.noreply.github.com>
2023-01-11 02:49:36 -0600

    Fixed

--- src/pages/Base/index.js
+++ src/pages/Base/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Base.css";
 import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { Col, Row } from "react-bootstrap";
 const buttonVariants = {
   hover: {
